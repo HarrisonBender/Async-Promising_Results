51334772e865db31c6c0fc0dcae41b175fe20a30
HarrisonBender <83611772+HarrisonBender@users.noreply.github.com>
2021-06-28 13:19:22 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -31,3 +31,24 @@ slowMath.add(6, 2)
         console.log("There has been an error in the script.")
         console.log(err);
     });
+
+    //Async 
+    async function newVersion(x, y) {
+        try {
+            const a = await slowMath.add(x, y);
+            const b = await slowMath.multiply(a, 2);
+            const c = await slowMath.divide(b, 4);
+            const d = await slowMath.subtract(c, 3);
+            const e = await slowMath.add(d, 98);
+            const f = await slowMath.remainder(e, 2);
+            const g = await slowMath.multiply(f, 50);
+            const h = await slowMath.remainder(g, 40);
+            const i = await slowMath.add(h, 32);
+            return console.log(i);
+        }
+        catch (err) {
+            console.log(`Error: ${err}`);
+        }
+    }
+    
+    newVersion(6, 2);
